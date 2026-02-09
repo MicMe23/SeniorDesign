a80a1859d672e0c377bb9ded708d932cbd8a0cc0
UCvargasjp <vargasjp@mail.uc.edu>
2026-02-09 11:00:06 -0500

    cartesian plane is made and working

--- application/diagram_gen/js/cartesianGraph.js
+++ application/diagram_gen/js/cartesianGraph.js
@@ -5,9 +5,8 @@ class CartesianGraph {
       parentElement: _config.parentElement,
       containerWidth: _config.containerWidth || 1000,
       containerHeight: _config.containerHeight || 1000,
-      margin: {top: 50, right: 50, bottom: 50, left: 50},
+      margin: {top: 20, right: 20, bottom: 20, left: 20},
       tooltipPadding: _config.tooltipPadding || 15
-
     }
 
     this.data = _data; 
@@ -15,15 +14,9 @@ class CartesianGraph {
     this.initVis();
   }
 
-  initVis() {
+    initVis() {
       //setting up the chart- things that won't need to update on user actions
 
-      console.log("Let's draw a chart!!");
-
-      // I recommend avoiding simply using the this keyword within complex class code
-      // involving SVG elements because the scope of this will change and it will cause
-      // undesirable side-effects. Instead, we recommend creating another variable at
-      // the start of each function to store the this-accessor
       let vis = this; 
   
 
@@ -31,7 +24,6 @@ class CartesianGraph {
       vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
       vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
 
-      // Define 'svg' as a child-element (g) from the drawing area and include spaces
       // Add <svg> element (drawing space)
       vis.svg = d3.select(vis.config.parentElement)
           .attr('width', vis.config.containerWidth)
@@ -40,94 +32,54 @@ class CartesianGraph {
       vis.chart = vis.svg.append('g')
           .attr('transform', `translate(${vis.config.margin.left}, ${vis.config.margin.top})`);
 
-      // Initialize linear and ordinal scales (input domain and output range)
+      // Initialize linear scales
       vis.xScale = d3.scaleLinear()
         .domain([-20, 20])
         .range([0, vis.width]);
 
-
       vis.yScale = d3.scaleLinear()
-        .domain([d3.max(vis.data, d => d.year), d3.min( vis.data, d => d.year)]) 
-        .range([0, vis.height]);
+        .domain([-20, 20]) 
+        .range([vis.height, 0]);
 
+      // ---- GRIDLINES ----
+      // Draw gridlines from the chart bounds so they fill ALL quadrants
 
-      vis.rScale = d3.scaleLinear()
-        .domain(d3.extent(vis.data, d=> d.cost))
-        .range([5, 100]);
+      vis.xGrid = d3.axisBottom(vis.xScale)
+        .ticks(40)
+        .tickSize(-vis.height)
+        .tickFormat('');
 
+      vis.xGridGroup = vis.chart.append('g')
+        .attr('class', 'grid x-grid')
+        .attr('transform', `translate(0, ${vis.height})`)
+        .call(vis.xGrid);
 
-      // Construct a new ordinal scale with a range of ten categorical colours
-      vis.colorPalette = d3.scaleOrdinal(d3.schemeTableau10);
-      vis.colorPalette.domain( "tropical-cyclone", "drought-wildfire", "severe-storm", "flooding" );
+      vis.yGrid = d3.axisLeft(vis.yScale)
+        .ticks(40)
+        .tickSize(-vis.width)
+        .tickFormat('');
 
+      vis.yGridGroup = vis.chart.append('g')
+        .attr('class', 'grid y-grid')
+        .attr('transform', `translate(0, 0)`)
+        .call(vis.yGrid);
 
-        // Initialize axes
-        vis.xAxis = d3.axisTop(vis.xScale);
-        vis.yAxis = d3.axisLeft(vis.yScale);
+      // ---- AXES ----
+      // Center the axes at (0,0) and draw them on top of the grid
 
+      vis.xAxis = d3.axisBottom(vis.xScale);
+      vis.yAxis = d3.axisLeft(vis.yScale);
 
-        // Draw the axis
-        vis.xAxisGroup = vis.chart.append('g')
-          .attr('class', 'axis x-axis') 
-          .call(vis.xAxis);
+      vis.xAxisGroup = vis.chart.append('g')
+        .attr('class', 'axis x-axis') 
+        .attr('transform', `translate(0, ${vis.yScale(0)})`)
+        .call(vis.xAxis);
 
-        vis.yAxisGroup = vis.chart.append('g')
-          .attr('class', 'axis y-axis')
-          .call(vis.yAxis);
+      vis.yAxisGroup = vis.chart.append('g')
+        .attr('class', 'axis y-axis')
+        .attr('transform', `translate(${vis.xScale(0)}, 0)`)
+        .call(vis.yAxis);
 
-      vis.updateVis(); //call updateVis() at the end - we aren't using this yet. 
+      vis.updateVis(); 
   }
-
-
- updateVis() {
-  let vis = this;
-
-
-     //Add circles for each event in the data
-
-     //TO DO- how can you change this so that it updates, based on the new data
-     //  if some elements have been removed or added, we need to use 'join' rather than enter and append
-      vis.circles = vis.chart.selectAll('circle')
-          .data(vis.data)
-          .enter()
-          .append('circle')
-         .attr('fill', (d) => vis.colorPalette(d.category) )
-          .attr('opacity', .8)
-          .attr('stroke', "gray")
-          .attr('stroke-width', 2)
-          .attr('r', (d) => vis.rScale(d.cost) ) 
-          .attr('cy', (d) => vis.yScale(d.year) ) 
-          .attr('cx',(d) =>  vis.xScale(d.daysFromYrStart) );;
-
-        vis.circles
-          .on('mouseover', (event,d) => {
-            console.log("mouse over! ");
-            console.log(event);
-            console.log(d);
-          
-          d3.select('#tooltip')
-            .style('display', 'block')
-            .style('left', (event.pageX + vis.config.tooltipPadding) + 'px')   
-            .style('top', (event.pageY + vis.config.tooltipPadding) + 'px')
-            .html(`
-              <div class="tooltip-title">${d.name}</div>
-              <ul>
-                <li>Year: ${d.year}</li>
-                <li>Cost in Billions $: ${d.cost}</li>
-              </ul>
-            `);
-        })
-        .on('mouseleave', () => {
-          d3.select('#tooltip').style('display', 'none');
-        });
-         
-
- }
-
- renderVis() { 
-
-  }
-
-
-
 }
